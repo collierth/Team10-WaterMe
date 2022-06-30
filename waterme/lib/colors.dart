cf3f16a54aeaa3ce6415a9f555227297802d40d5
Collier Hawkins <collier_hawkins@bellsouth.net>
2022-06-29 23:25:23 -0400

    Home Page UI Part 1

---
+++ waterme/lib/colors.dart
@@ -0,0 +1,11 @@
+import "dart:ui";
+
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static Color homePageBackground=const Color(0xFFfbfcff);
+  static Color MidnightBlue = const Color(0xFF145DA0);
+  static Color DarkBlue = const Color(0xFF0c2D48);
+  static Color Blue = const Color(0xFF2E8BC0);
+  static Color BabyBlue = const Color(0xFFB1D4E0);
+}
---
+++ waterme/lib/home_page.dart
@@ -0,0 +1,90 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'colors.dart' as color;
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String greeting = 'Hello';
+  var dt = DateTime.now();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: color.AppColor.homePageBackground,
+      body: Container(
+        padding: const EdgeInsets.only(top: 70, left: 30, right: 30),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text(
+                  greeting,
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w700
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 30,),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 220,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    color.AppColor.MidnightBlue,
+                    color.AppColor.BabyBlue,
+                  ],
+                  begin: Alignment.bottomLeft,
+                  end: Alignment.centerRight, 
+                ),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                  topRight: Radius.circular(80),
+                ), 
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(10, 10),
+                    blurRadius: 20,
+                    color: Colors.black.withOpacity(0.5)
+                  )
+                ]
+              ),
+              child: Container(
+                padding: const EdgeInsets.only(left: 20, top: 25,),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Welcome to Water Me!",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                )
+              )
+              
+            )
+          ]
+        ),
+      ),
+    );
+  }
+
+}
